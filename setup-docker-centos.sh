79083a1936e8e5f173bf3b555f5c948c5d5e0beb
Terry Leung <ttoleung@gmail.com>
2017-08-17 02:02:13 +0000

    added docker setup for centos

---
+++ setup-docker-centos.sh
@@ -0,0 +1,7 @@
+sudo yum install git
+sudo yum install wget
+wget https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-17.06.0.ce-1.el7.centos.x86_64.rpm
+sudo yum install docker-ce-17.06.0.ce-1.el7.centos.x86_64.rpm
+sudo docker run hello-world
+git config --global user.email "ttoleung@gmail.com"
+git config --global user.name "Terry Leung"
